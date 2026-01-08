42a010c5a31ef9033e865386f8ec57efa170228e
Arunprasad Sampath <61770847+arun2701@users.noreply.github.com>
2026-01-08 14:39:16 +0530

    Create security group for terra EC2 instance
    Added a new security group resource for EC2 instances.

--- sg_test.tf
+++ sg_test.tf
@@ -1,1 +1,7 @@
-resource "aws_security_group" "test1" {}
+resource "aws_security_group" "hcp_terra_test_sg" {
+    name        = "hcp_terra_test_sg"
+    description = "Security group for terra EC2 instance"
+    tags = {
+        Name = "terra_sg"
+    }
+}
