34a98bd98f7857bc9dc3e4db09c56ecd1418333f
Arunprasad Sampath <61770847+arun2701@users.noreply.github.com>
2026-01-12 17:39:52 +0530

    Add AWS EC2 instance resource to sg_test.tf

--- sg_test.tf
+++ sg_test.tf
@@ -5,3 +5,8 @@ resource "aws_security_group" "hcp_terra_test_sg" {
         Name = "terra_sg_VCS_workflow_1"
     }
 }
+
+resource "aws_instance" "myec2" {
+   ami = "ami-07ff62358b87c7116"
+   instance_type = "t2.micro"
+
