990546577ffc32f30a17c8b8f4682a27941c7133
Arunprasad Sampath <61770847+arun2701@users.noreply.github.com>
2026-01-12 17:50:36 +0530

    Add tags to AWS EC2 instance resource

--- sg_test.tf
+++ sg_test.tf
@@ -9,5 +9,7 @@ resource "aws_security_group" "hcp_terra_test_sg" {
 resource "aws_instance" "myec2" {
    ami = "ami-07ff62358b87c7116"
    instance_type = "t2.micro"
-
+   tags = {
+        Name = "hcp_ec2"
+    }
 }
